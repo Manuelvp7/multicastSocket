2ef08fbae90bd7e7c84640e5bed5b77bdf13dfc4
Manuelvp7 <jmanuelvp@outlook.es>
2019-11-07 11:12:36 -0600

    Client server added

---
+++ multicastClient.cpp
@@ -0,0 +1,35 @@
+#include "SocketMulticast.h"
+#include "Solicitud.h"
+#include "PaqueteDatagrama.h"
+#include "mensaje.h"
+#include <cstring>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+
+	SocketMulticast socketMulticast = SocketMulticast(atoi(argv[2]));
+	Solicitud solicitud = Solicitud();
+
+	PaqueteDatagrama package = PaqueteDatagrama();
+
+	printf("UNIENDOSE %s\n",argv[1] );
+
+	char *a;
+
+	socketMulticast.unirseGrupo(argv[1]);
+	while(true){
+		socketMulticast.recibe(&package);
+		printf("RESPONDER A: %s %d\n",package.obtieneDireccion(),package.obtienePuerto() );
+		a = solicitud.doOperation(package.obtieneDireccion(),package.obtienePuerto(),1,"HOLA QUE PASO");
+		//socketDatagrama.envia(package);
+
+		
+		//printf("HOLASSS\n");
+
+
+	}
+	
+	/* code */
+	return 0;
+}
