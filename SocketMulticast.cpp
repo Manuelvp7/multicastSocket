9cfc00a9188de2ceb5fcd804dcc25c35879c23e2
manuel <manuel@localhost.localdomain>
2019-11-07 12:21:08 -0500

    socketMulticastIplementation created

---
+++ SocketMulticast.cpp
@@ -0,0 +1,94 @@
+#include <sys/types.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <net/if.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <iostream>
+#include <arpa/inet.h>
+#include <cstring>
+#include "SocketMulticast.h"
+
+using namespace std;
+
+SocketMulticast::SocketMulticast(int port){
+    //s = socket(AF_INET, SOCK_DGRAM, 0);
+    s = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    // int reuse = 1;
+    // if (setsockopt(s, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) == -1) {
+    //     printf("Error al llamar a la función setsockopt\n");
+    //     exit(0);
+    // }
+    // unsigned char TTL = "1";
+    struct sockaddr_in direccionLocal;
+    bzero((char *)&direccionLocal, sizeof(direccionLocal));
+    direccionLocal.sin_family = AF_INET;
+    direccionLocal.sin_addr.s_addr = INADDR_ANY;
+    direccionLocal.sin_port = htons(port);
+    bind(s, (struct sockaddr *)&direccionLocal, sizeof(direccionLocal));
+}
+
+SocketMulticast::~SocketMulticast(){
+    shutdown(s, SHUT_RDWR);
+}
+
+int SocketMulticast::recibe(PaqueteDatagrama *p){
+
+    struct sockaddr_in direccionForanea;
+    char * respuesta = (char*)malloc(sizeof(char)*4000);
+    unsigned int lData = sizeof(direccionForanea);
+    int responseStatus = recvfrom(s, respuesta, 4000, 0, (struct sockaddr *)&direccionForanea, &lData);
+    p->inicializaArgumentosMensaje(respuesta);
+    char str[INET_ADDRSTRLEN];
+    struct sockaddr_in* ip = (struct sockaddr_in*)&direccionForanea;
+    struct in_addr ipAdd = ip->sin_addr;
+    unsigned short ipPort = ip->sin_port;
+    inet_ntop(AF_INET, &ipAdd, str, INET_ADDRSTRLEN);
+    printf("IP: %s  \n", str);
+    printf("Puerto: %d \n", (int) ntohs(ip->sin_port));
+    printf(" %s\n",p->obtieneDatos() );
+    p->inicializaIP(inet_ntoa(direccionForanea.sin_addr));
+    p->inicializaPuerto(ntohs(direccionForanea.sin_port));
+    return 1;
+}
+
+int SocketMulticast::envia(PaqueteDatagrama * p, unsigned char ttl){
+
+    int validator  = setsockopt(s, IPPROTO_IP, IP_MULTICAST_TTL, (void *) &ttl, sizeof(ttl));
+    if (validator<0){
+        printf("An error has occurred\n Exiting...");
+        exit(0);
+    }else{
+        //printf("Emmiter is up and running\n Sending messages ");
+    }   
+
+    struct sockaddr_in direccionForanea;
+    int enviado;
+    direccionForanea.sin_family = AF_INET;
+    direccionForanea.sin_addr.s_addr = inet_addr(p->obtieneDireccion());
+    direccionForanea.sin_port = htons(p->obtienePuerto());
+    unsigned int lData = sizeof(direccionForanea);
+    enviado = sendto(s, p->obtieneDatos(), p->obtieneLongitud(), 0, (struct sockaddr *)&direccionForanea, lData);
+    return enviado;
+}
+
+void SocketMulticast::unirseGrupo(char * multicastIP){
+
+    struct ip_mreq multicast;
+    multicast.imr_multiaddr.s_addr = inet_addr(multicastIP);
+    multicast.imr_interface.s_addr = htonl(INADDR_ANY);
+    if (setsockopt(s, IPPROTO_IP, IP_ADD_MEMBERSHIP, (void *) &multicast, sizeof(multicast)) < 0){
+         printf("ERROR AL UNIRSE AL GRUPO MULTICAST \n");
+         exit(0);
+     } 
+
+}
+void SocketMulticast::salirseGrupo(char * multicastIP){
+
+    struct ip_mreq multicast;
+    multicast.imr_multiaddr.s_addr = inet_addr(multicastIP);
+    multicast.imr_interface.s_addr = htonl(INADDR_ANY);
+    setsockopt(s, IPPROTO_IP, IP_DROP_MEMBERSHIP, (void *) &multicast, sizeof(multicast));
+
+}
